5f237c7f96110f8fd55cd1f8e8636d3e5f75d17e
arefmaghdam <arefmaghdam@gmail.com>
2023-01-30 00:46:47 +0330

    feat: added boxes inside layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,35 +4,29 @@ const Layout = () => {
   return (
     <div className={`container-fluid ${styles.container}`}>
       <div className="row">
-        <div className={`col-sm-12 ${styles.header}`}>header</div>
+        <div className={`col-sm-12 ${styles.header}`}>
+          <div className={`${styles.headerContent} ${styles.color1}`}>
+            header
+          </div>
+        </div>
       </div>
       <div className="row ">
-        <div
-          className={`col-sm-4 ${styles.box} ${styles.color1} ${styles.watchlist}`}
-        >
-          watchlist
+        <div className={`col-sm-4  ${styles.watchlist}`}>
+          <div className={`${styles.box} ${styles.color1}`}>watchlist</div>
         </div>
-        <div
-          className={`col-sm-3 ${styles.box} ${styles.color1} ${styles.details}`}
-        >
-          details
+        <div className={`col-sm-3  ${styles.details}`}>
+          <div className={`${styles.box} ${styles.color1}`}>details</div>
         </div>
-        <div
-          className={`col-sm-5 ${styles.box} ${styles.color1} ${styles.orderbook}`}
-        >
-          orderbook
+        <div className={`col-sm-5  ${styles.orderbook}`}>
+          <div className={`${styles.box} ${styles.color1}`}>orderbook</div>
         </div>
       </div>
       <div className="row">
-        <div
-          className={`col-sm-4 ${styles.box} ${styles.color2} ${styles.overview}`}
-        >
-          overview
+        <div className={`col-sm-4 ${styles.overview}`}>
+          <div className={`${styles.box} ${styles.color2}`}>overview</div>
         </div>
-        <div
-          className={`col-sm-8 ${styles.box} ${styles.color2} ${styles.report}`}
-        >
-          report
+        <div className={`col-sm-8  ${styles.report}`}>
+          <div className={`${styles.box} ${styles.color2}`}>report</div>
         </div>
       </div>
     </div>
