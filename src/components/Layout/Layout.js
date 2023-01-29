8e5af6ac47c25b69ca7d0272907bc0b21dce16af
arefmaghdam <arefmaghdam@gmail.com>
2023-01-30 00:03:55 +0330

    feat: added layout using bootstrap grid system

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,42 @@
+import styles from "./Layout.module.css";
+
 const Layout = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className={`container-fluid ${styles.container}`}>
+      <div className="row">
+        <div className={`col-sm-12 ${styles.header}`}>header</div>
+      </div>
+      <div className="row ">
+        <div
+          className={`col-sm-4 ${styles.box} ${styles.color1} ${styles.watchlist}`}
+        >
+          watchlist
+        </div>
+        <div
+          className={`col-sm-3 ${styles.box} ${styles.color1} ${styles.details}`}
+        >
+          details
+        </div>
+        <div
+          className={`col-sm-5 ${styles.box} ${styles.color1} ${styles.orderbook}`}
+        >
+          orderbook
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className={`col-sm-4 ${styles.box} ${styles.color2} ${styles.overview}`}
+        >
+          overview
+        </div>
+        <div
+          className={`col-sm-8 ${styles.box} ${styles.color2} ${styles.report}`}
+        >
+          report
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
