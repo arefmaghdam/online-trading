b9792d3932f22cb9611a40cceef3debcdce39cfa
arefmaghdam <arefmaghdam@gmail.com>
2023-01-31 23:57:34 +0330

    feat: added time and status icon to the project

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,15 @@
 import styles from "./User.module.css";
 import { FaUserCircle } from "react-icons/fa";
+import { MdDesktopWindows } from "react-icons/md";
 
 const User = () => {
   return (
     <div className={`${styles.user}`}>
+      <h4>
+        {" "}
+        <MdDesktopWindows />{" "}
+      </h4>
+      <h6 className={`${styles.time}`}>12:47</h6>
       <h3>
         {" "}
         <FaUserCircle />{" "}
