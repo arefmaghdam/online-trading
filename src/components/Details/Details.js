8150a5d3db1b020ee083395c6b6c4793b94aad10
arefmaghdam <arefmaghdam@gmail.com>
2023-02-04 16:02:19 +0330

    feat: added header features for detail component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,9 +2,19 @@ import React from "react";
 import styles from "./Details.module.css";
 
 const Details = () => {
-    return (
-        <div>details start</div>
-    )
-}
+  return (
+    <div>
+      <div className={styles.detailsHeader}>
+        <div className={styles.coin}>
+            <div>BTC-USDT</div>
+            <span>15.715<p>+15.2%</p></span>
+        </div>
+        <div className={styles.buySellButton}>
+           <button className="btn btn-warning text-white font-weight-bold">Buy/Sell</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Details
+export default Details;
