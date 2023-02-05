f696f3134cf0ec3ca8d7c2698bfb53c663029d10
arefmaghdam <arefmaghdam@gmail.com>
2023-02-05 13:44:24 +0330

    feat: added settings for layout of table andd chart in report component

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -2,11 +2,17 @@ import styles from "./Report.module.css";
 
 const Report = () => {
   return (
-    <div className={styles.reportMenu}>
-      <ul className={styles.report}>
-      <li>Orders</li>
-      <li>Transactions</li>
-    </ul>
+    <div>
+      <div className={styles.reportMenu}>
+        <ul className={styles.report}>
+          <li>Orders</li>
+          <li>Transactions</li>
+        </ul>
+      </div>
+      <div className={styles.reportContent}>
+        <div className={styles.table}>Table</div>
+        <div className={styles.barChart}>Chart</div>
+      </div>
     </div>
   );
 };
