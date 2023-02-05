eb345cccc08e8458373c962aa2d81a25b3a581b2
arefmaghdam <arefmaghdam@gmail.com>
2023-02-05 13:29:53 +0330

    feat: added report menu items

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,9 +1,14 @@
 import styles from "./Report.module.css";
 
 const Report = () => {
-  return (<div className={styles.reportMenu}>
-
-  </div>);
+  return (
+    <div className={styles.reportMenu}>
+      <ul className={styles.report}>
+      <li>Orders</li>
+      <li>Transactions</li>
+    </ul>
+    </div>
+  );
 };
 
 export default Report;
