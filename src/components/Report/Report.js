dbd4dbac81b3ef42092738b6bf8caf0bc2d4746d
arefmaghdam <arefmaghdam@gmail.com>
2023-02-06 21:17:23 +0330

    fix: added scrollbar to report component

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -64,6 +64,42 @@ const Report = () => {
                   <ProgressBar />
                 </td>
               </tr>
+              <tr>
+                <td>SHIBA-USDT</td>
+                <td>751</td>
+                <td>2.35126</td>
+                <td>Sell</td>
+                <td>23-12-2022 17:45 AM</td>
+                <td>24-12-2022 21:28 AM</td>
+                <td>Pending</td>
+                <td>
+                  <ProgressBar />
+                </td>
+              </tr>
+              <tr>
+                <td>SHIBA-USDT</td>
+                <td>751</td>
+                <td>2.35126</td>
+                <td>Sell</td>
+                <td>23-12-2022 17:45 AM</td>
+                <td>24-12-2022 21:28 AM</td>
+                <td>Pending</td>
+                <td>
+                  <ProgressBar />
+                </td>
+              </tr>
+              <tr>
+                <td>SHIBA-USDT</td>
+                <td>751</td>
+                <td>2.35126</td>
+                <td>Sell</td>
+                <td>23-12-2022 17:45 AM</td>
+                <td>24-12-2022 21:28 AM</td>
+                <td>Pending</td>
+                <td>
+                  <ProgressBar />
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
