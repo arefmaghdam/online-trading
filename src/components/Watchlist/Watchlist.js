ca6d683c80e93f9a701cc27d3cc5bfeb067cd6de
arefmaghdam <arefmaghdam@gmail.com>
2023-02-06 21:37:46 +0330

    fix: added scrollbar to watchlist component

--- src/components/Watchlist/Watchlist.js
+++ src/components/Watchlist/Watchlist.js
@@ -7,31 +7,38 @@ import { MdRemove } from "react-icons/md";
 const Watchlist = () => {
   return (
     <div className={`container-fluid ${styles.watchlistSize}`}>
-      <div className="row">
+      <div className={`row ${styles.watchlist} ${styles.titleContainer}`}>
         <div className="col-md-12">
-          <div className={`${styles.watchlist}`}>
-            <span className={`${styles.watchlistHeader}`}>Watchlist</span>
-
-            <div class={styles.toolbar}>
-              <div class={styles.select}>
-                <select>
-                  <option>BTC-USDT</option>
-                  <option>SHIBA-USDT</option>
-                  <option>ETC-USDT</option>
-                </select>
-              </div>
-              <div>
-                <button className={`btn ${styles.editWatchlist}`}>
-                  <MdEdit />
-                </button>
-                <button className={`btn ${styles.addWatchlist}`}>
-                  <MdAdd />
-                </button>
-                <button className={`btn ${styles.removeWatchlist}`}>
-                  <MdRemove />
-                </button>
-              </div>
+          <span className={`${styles.watchlistHeader}`}>Watchlist</span>
+        </div>
+      </div>
+      <div className={`row ${styles.watchlist} ${styles.toolbarContainer}`}>
+        <div className="col-md-12">
+          <div class={styles.toolbar}>
+            <div class={styles.select}>
+              <select>
+                <option>BTC-USDT</option>
+                <option>SHIBA-USDT</option>
+                <option>ETC-USDT</option>
+              </select>
             </div>
+            <div>
+              <button className={`btn ${styles.editWatchlist}`}>
+                <MdEdit />
+              </button>
+              <button className={`btn ${styles.addWatchlist}`}>
+                <MdAdd />
+              </button>
+              <button className={`btn ${styles.removeWatchlist}`}>
+                <MdRemove />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`row ${styles.watchlist} ${styles.tableContainer}`}>
+        <div className="col-md-12">
+          <div>
             <table className={`table ${styles.table}`}>
               <thead>
                 <tr>
@@ -42,6 +49,96 @@ const Watchlist = () => {
                 </tr>
               </thead>
               <tbody>
+                <tr>
+                  <td>
+                    BTC<div>(USDT)</div>
+                  </td>
+                  <td>16.2564</td>
+                  <td>16.2357</td>
+                  <td>
+                    15.2368<p>+15.2%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    SHIBA<div>(USDT)</div>
+                  </td>
+                  <td>182.36542</td>
+                  <td>182.58791</td>
+                  <td>
+                    182.59473<p>+1.75%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    ETC<div>(USDT)</div>
+                  </td>
+                  <td>25.32657</td>
+                  <td>25.25987</td>
+                  <td>
+                    25.32657<p>+7%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    BTC<div>(USDT)</div>
+                  </td>
+                  <td>16.2564</td>
+                  <td>16.2357</td>
+                  <td>
+                    15.2368<p>+15.2%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    SHIBA<div>(USDT)</div>
+                  </td>
+                  <td>182.36542</td>
+                  <td>182.58791</td>
+                  <td>
+                    182.59473<p>+1.75%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    ETC<div>(USDT)</div>
+                  </td>
+                  <td>25.32657</td>
+                  <td>25.25987</td>
+                  <td>
+                    25.32657<p>+7%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    BTC<div>(USDT)</div>
+                  </td>
+                  <td>16.2564</td>
+                  <td>16.2357</td>
+                  <td>
+                    15.2368<p>+15.2%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    SHIBA<div>(USDT)</div>
+                  </td>
+                  <td>182.36542</td>
+                  <td>182.58791</td>
+                  <td>
+                    182.59473<p>+1.75%</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    ETC<div>(USDT)</div>
+                  </td>
+                  <td>25.32657</td>
+                  <td>25.25987</td>
+                  <td>
+                    25.32657<p>+7%</p>
+                  </td>
+                </tr>
                 <tr>
                   <td>
                     BTC<div>(USDT)</div>
