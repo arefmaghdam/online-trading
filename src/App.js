5560430815dfee7ca73c8eccb1392a5011c85b22
arefmaghdam <arefmaghdam@gmail.com>
2023-02-15 13:39:11 +0330

    feat: created routes in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import SignInResult from "./components/SignInResult/SignInResult";
 
 const App = () => {
-  return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}/>
+        <Route path="/signin-result" element={<SignInResult />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
