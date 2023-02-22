a159a191c75cb2283bbf550fdde26367d53bf374
arefmaghdam <arefmaghdam@gmail.com>
2023-02-22 20:44:17 +0330

    feat: sent request using useGetAPI

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,36 +1,16 @@
 import { Typeahead } from "react-bootstrap-typeahead";
 import styles from "./Search.module.css";
 import "react-bootstrap-typeahead/css/Typeahead.min.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import useGetAPI from "../../hooks/useGetAPI";
 
 const Search = () => {
-  const [symbols, setSymbols] = useState([]);
-
-  useEffect(() => {
-    const token = localStorage.getItem("currentToken");
-
-    axios
-      .get("https://ot-api.eltak.ir/MarketData/Symbol/Lightweight", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        setSymbols(response.data.data);
-      })
-      .catch((err) => {
-        if (err.response.status === 401)
-          window.location.href = "https://ot-api.eltak.ir/bff/login";
-      });
-  }, []);
-
+  const [lightweightSymbols] = useGetAPI('https://ot-api.eltak.ir/MarketData/Symbol/Lightweight', [])
   return (
     <Typeahead
       id="searchInput"
       className={styles.search}
       labelKey={(option) => option.symbolId}
-      options={symbols}
+      options={lightweightSymbols}
       size="sm"
     />
   );
--- src/hooks/useGetAPI.js
+++ src/hooks/useGetAPI.js
@@ -1,2 +1,26 @@
-useGetAPI = () => {};
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useGetAPI = (apiUrl, defaultValue) => {
+  const [symbols, setSymbols] = useState([]);
+  useEffect(() => {
+    const token = localStorage.getItem("currentToken");
+    axios
+      .get(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setSymbols(response.data.data);
+        console.log(symbols);
+      })
+      .catch((err) => {
+        if (err.response.status === 401)
+          window.location.href = "https://ot-api.eltak.ir/bff/login";
+      });
+  }, []);
+
+  return [symbols];
+};
 export default useGetAPI;
