9c835773fe8ced0abe5990f9e7d770e0918643e6
arefmaghdam <arefmaghdam@gmail.com>
2023-02-25 23:57:33 +0330

    feat: implemented useOrderbook hook

--- src/hooks/useOrderbook.js
+++ src/hooks/useOrderbook.js
@@ -1,2 +1,12 @@
-const useOrderbook = () => {};
+import { useEffect } from "react";
+import useGetAPI from "./useGetAPI";
+
+const useOrderbook = () => {
+  const [orderbook] = useGetAPI(
+    "https://ot-api.eltak.ir/MarketData/Bestlimit/BTC-USDT",
+    {}
+  );
+
+  return [orderbook];
+};
 export default useOrderbook;
