3b8b6937688bb38141330cd54c375f38c94302d4
arefmaghdam <arefmaghdam@gmail.com>
2023-02-25 23:57:49 +0330

    feat: implemented orderbook component

--- src/components/Orderbook/Orderbook.js
+++ src/components/Orderbook/Orderbook.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import useOrderbook from "../../hooks/useOrderbook";
 import styles from "./Orderbook.module.css";
 
 const Orderbook = () => {
+  const [orderbook] = useOrderbook({});
+  const [orderbookData, setOrderbookData] = useState([]);
+  useEffect(() => {
+    if (orderbook.bids == undefined) return;
+    let askBid = [];
+    let bids = orderbook.bids;
+    let asks = orderbook.asks;
+    for (let item = 0; item < bids.length; item++) {
+      const data = {
+        bid: bids[item],
+        ask: asks[item],
+      };
+      askBid.push(data);
+    }
+    setOrderbookData(askBid);
+  }, [orderbook]);
   return (
     <div className={styles.Orderbook}>
       <table className={`table ${styles.table}`}>
