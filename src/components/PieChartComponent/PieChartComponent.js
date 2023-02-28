38f131ebde7ca8f1e08691966db9ac04947fab59
arefmaghdam <arefmaghdam@gmail.com>
2023-02-28 19:02:05 +0330

    feat: implemented pieChart items using custom hook completely

--- src/components/PieChartComponent/PieChartComponent.js
+++ src/components/PieChartComponent/PieChartComponent.js
@@ -2,38 +2,34 @@ import { useEffect, useState } from "react";
 import Chart from "react-google-charts";
 import useOverview from "../../hooks/useOverview";
 import styles from "./PieChartComponent.module.css";
-const pieData = [
-  ["Items", "Markets"],
-  ["Blocked", 11],
-  ["Withdrawable", 5],
-  ["Pending", 7],
-  ["Profit/Loss", 2],
-];
-const pieOptions = {
-  pieHole: 0,
-  backgroundColor: "#3D3D3D",
-  chartArea: {
-    left: "1%",
-    top: "3%",
-    height: "74%",
-    width: "100%",
-  },
-  legend: { alignment: "center", textStyle: { color: "white" } },
-  slices: {
-    0: { color: "#fa6161" },
-    1: { color: "rgb(70, 189, 6)" },
-    2: { color: "#fbc223" },
-    3: { color: "#248bf3" },
-  },
-};
 
 const PieChartComponent = () => {
   const [overview] = useOverview({});
   const [overviewData, setOverviewData] = useState([]);
   useEffect(() => {
     if (overview == undefined) return;
-    console.log(overview);
+    setOverviewData(overview);
   }, [overview]);
+  const pieData = [
+    ["Items", "Markets"],
+    ["Blocked", overviewData.blocked],
+    ["Withdrawable", overviewData.withdrawable],
+  ];
+  const pieOptions = {
+    pieHole: 0,
+    backgroundColor: "#3D3D3D",
+    chartArea: {
+      left: "1%",
+      top: "3%",
+      height: "74%",
+      width: "100%",
+    },
+    legend: { alignment: "center", textStyle: { color: "white" } },
+    slices: {
+      0: { color: "#fa6161" },
+      1: { color: "rgb(70, 189, 6)" }
+    },
+  };
   return (
     <div className={styles.chart}>
       <Chart
