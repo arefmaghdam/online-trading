05338f4689d56e251cbf5f7b1481bf9036b8e6e2
arefmaghdam <arefmaghdam@gmail.com>
2023-03-01 23:22:17 +0330

    feat: displayed report component inside order tab in layout component and changed styles for matching with new changes

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -11,50 +11,42 @@ const Report = () => {
     setOredrsData(orders);
   }, [orders]);
   return (
-    <div className={styles.reportComponent}>
-      <div className={styles.reportMenu}>
-        <ul className={styles.report}>
-          <li>Orders</li>
-          <li>Transactions</li>
-        </ul>
-      </div>
-      <div className={styles.reportContent}>
-        <div className={styles.table}>
-          <table className={`table ${styles.tableContent}`}>
-            <thead>
-              <tr>
-                <th>Symbol</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Order Side</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-                <th>Status</th>
-                <th>
-                  <div>Progress</div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {ordersData.map((item) => {
-                return (
-                  <tr key={item.id}>
-                    <td>{item.symbolId}</td>
-                    <td>{item.quantity}</td>
-                    <td>{item.price}</td>
-                    <td>{item.orderSide}</td>
-                    <td>{item.createdAt}</td>
-                    <td>{item.updatedAt}</td>
-                    <td>{item.status}</td>
-                    <td>
-                      <ProgressBar />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+    <div className={styles.reportContent}>
+      <div className={styles.table}>
+        <table className={`table ${styles.tableContent}`}>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Order Side</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Status</th>
+              <th>
+                <div>Progress</div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {ordersData.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.symbolId}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price}</td>
+                  <td>{item.orderSide}</td>
+                  <td>{item.createdAt}</td>
+                  <td>{item.updatedAt}</td>
+                  <td>{item.status}</td>
+                  <td>
+                    <ProgressBar />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
