7a0697e4d18d508e59cd3a6475a873b13a16112e
arefmaghdam <arefmaghdam@gmail.com>
2023-03-05 12:52:53 +0330

    feat: created and initialized tbale in Trades component and set css styles

--- src/components/Trades/Trades.js
+++ src/components/Trades/Trades.js
@@ -1,5 +1,35 @@
+import styles from "./Trades.module.css";
+
 const Trades = () => {
-  return <div>This is trades component</div>;
+  return (
+    <>
+    <div className={styles.container}>
+        <div className={styles.orders}>Orders</div>
+        <div className={styles.trades}>Trades</div>
+    </div>
+    <div className={styles.tradeContent}>
+        <div className={styles.table}>
+            <table className={`table ${styles.tableContent}`}>
+                <thead>
+                    <tr>
+                        <th>SymbolID</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>CreatedAt</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </>
+  )
 };
 
 export default Trades;
