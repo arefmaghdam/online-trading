b7a2b80aac7f4cb8e0eeecf68ef1896018da2f04
arefmaghdam <arefmaghdam@gmail.com>
2023-03-06 13:32:59 +0330

    feat: displayed customer profile informations from local storage and optimized css styles

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,13 +2,15 @@ import { FaUserCircle } from "react-icons/fa";
 import styles from "./Profile.module.css";
 
 const Profile = () => {
-    return (
-        <div className={`shadow-lg p-3 mb-5 bg-white rounded ${styles.profile}`}>
-          <div><FaUserCircle/></div>
-          <h6>Taha Mohammadi</h6>
-          <a href="">Log out</a>
-        </div>
-    )
-}
+  return (
+    <div className={`shadow-lg p-3 mb-5 bg-white rounded ${styles.profile}`}>
+      <div>
+        <FaUserCircle />
+      </div>
+      <h6>{`${localStorage.getItem("customerName")}  ${localStorage.getItem("customerFamily")}`}</h6>
+      <a href="">Log out</a>
+    </div>
+  );
+};
 
 export default Profile;
