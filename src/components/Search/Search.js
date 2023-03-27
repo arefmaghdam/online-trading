a9454dd73ae02e4c86a027b77d459ae1464a81d8
arefmaghdam <arefmaghdam@gmail.com>
2023-03-28 01:12:58 +0430

    feat: dispatch symbolId in search component for subscribing in other components

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,11 +2,26 @@ import { Typeahead } from "react-bootstrap-typeahead";
 import styles from "./Search.module.css";
 import "react-bootstrap-typeahead/css/Typeahead.min.css";
 import useLightweightSymbols from "../../hooks/useLightweightSymbols";
+import { useDispatch } from "react-redux";
+import { setSearchSymbol } from "../../redux/searchSelectedSymbolSlice";
+import { useEffect, useState } from "react";
 
 const Search = () => {
   const [symbols] = useLightweightSymbols();
+  const [selectedLightweight, setSelectedLightweight] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSearchSymbol(selectedLightweight));
+  }, [selectedLightweight]);
+
   return (
     <Typeahead
+      onChange={(selected) => {
+        if (selected.length == 0) return;
+
+        setSelectedLightweight(selected[0].symbolId);
+      }}
       id="searchInput"
       className={styles.search}
       labelKey={(option) => option.symbolId}
