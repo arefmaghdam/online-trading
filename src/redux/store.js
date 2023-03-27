300a05ecf04328af061ba7de62e3ef2961029da6
arefmaghdam <arefmaghdam@gmail.com>
2023-03-27 23:16:09 +0430

    feat: handled searchSelectedSymbolSlice in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import selectedSymbolReducer from "./selectedSymbolSlice";
+import searchSelectedSymbolReducer from "./searchSelectedSymbolSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     selectedSymbol: selectedSymbolReducer,
+    searchSelectedSymbol: searchSelectedSymbolReducer,
   },
 });
 
