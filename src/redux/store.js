8a98397a25ee6858ca86b1535a44aefac9708c2d
arefmaghdam <arefmaghdam@gmail.com>
2023-03-28 22:41:51 +0430

    feat: import sidebarStatusSlice in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import selectedSymbolReducer from "./selectedSymbolSlice";
 import searchSelectedSymbolReducer from "./searchSelectedSymbolSlice";
+import sidebarStatusReducer from "./sidebarStatusSlice"
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     selectedSymbol: selectedSymbolReducer,
     searchSelectedSymbol: searchSelectedSymbolReducer,
+    sidebarStatus: sidebarStatusReducer,
   },
 });
 
