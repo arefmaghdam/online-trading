95b8251e265462ecf5beb79d95e3ac8ce9e46773
arefmaghdam <arefmaghdam@gmail.com>
2023-04-02 01:31:31 +0430

    feat: subscribe to price updates on a new component (AppRouter.js)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import SignInResult from "./components/SignInResult/SignInResult";
-
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import AppRouter from "./AppRouter";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/signin-result" element={<SignInResult />} />
-          <Route path="/report" element={<SignInResult />} />
-          <Route path="/topup" element={<SignInResult />} />
-          <Route path="/management" element={<SignInResult />} />
-        </Routes>
-      </BrowserRouter>
+      <AppRouter />
     </Provider>
   );
 };
---
+++ src/AppRouter.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import * as signalR from "@microsoft/signalr";
+import { setPrice } from "./redux/signalRSlice";
+import { useDispatch } from "react-redux";
+import Layout from "./components/Layout/Layout";
+import SignInResult from "./components/SignInResult/SignInResult";
+import config from "./config";
+
+const AppRouter = function () {
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    let connection = new signalR.HubConnectionBuilder()
+      .withUrl(
+        `${config.OT_URL}Hubs/Default?tokenp=${localStorage.getItem(
+          "currentToken"
+        )}`
+      )
+      .build();
+
+    connection.on("ReceivePriceUpdate", (data) => {
+      dispatch(setPrice(data));
+    });
+
+    connection.start();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/signin-result" element={<SignInResult />} />
+        <Route path="/report" element={<SignInResult />} />
+        <Route path="/topup" element={<SignInResult />} />
+        <Route path="/management" element={<SignInResult />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
